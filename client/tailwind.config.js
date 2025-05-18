05dbb6b4b5adc2b9ccd48e0f4304685f9fdfad01
Iris Li <yushanli54@gmail.com>
2025-05-17 23:03:28 -0700

    add color palette to index.css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: { tan: "#ffe4d4", green: "#b2d2c3" }
+    }
+  },
+  plugins: [require('tailwind-scrollbar-hide'), require('@tailwindcss/forms')],
+}
+
   
