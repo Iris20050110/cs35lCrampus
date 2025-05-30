6c5b4ecd7ef45d1d836c2ebb1b20d002c29dabec
Iris Li <yushanli54@gmail.com>
2025-05-29 23:37:54 -0700

    wire up assignment page with authentication

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -3,10 +3,16 @@ import mongoose from "mongoose";
 //text: the task description
 //completed: whether it’s done or not
 //dueDate: when users want to complete the task
+//user: user that creates this task
 const todoSchema = new mongoose.Schema({
   text:      { type: String, required: true },
   completed: { type: Boolean, default: false },
-  dueDate:   { type: Date, required: true }
+  dueDate:   { type: Date, required: true },
+  user:      { 
+    type: String,
+    ref:  'User',
+    required: true
+  }
 }, { timestamps: true });
 
 const Todo = mongoose.model("Todo", todoSchema);
