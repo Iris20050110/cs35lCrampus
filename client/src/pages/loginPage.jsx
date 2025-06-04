ab43d42fb1d89a99648c0bd6a939c270d278112a
Iris Li <yushanli54@gmail.com>
2025-06-03 19:45:15 -0700

    adjust login styling

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -3,10 +3,10 @@ import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../components/navbar";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
-    checkAuthStatus();
+    checkAuthStatus()
   }, []);
 
   const checkAuthStatus = async () => {
@@ -14,34 +14,38 @@ const LoginPage = () => {
       const response = await fetch("http://localhost:5000/api/auth/check", {
         credentials: "include", // for cookies?
       });
-      const data = await response.json();
+      const data = await response.json()
 
       if (data.isAuthenticated) {
-        navigate("/");
+        navigate("/")
       }
     } catch (err) {
-      console.error("Error checking auth status:", err);
+      console.error("Error checking auth status:", err)
     }
   };
 
   const handleGoogleLogin = () => {
-    window.open("http://localhost:5000/api/auth/google", "_self");
+    window.open("http://localhost:5000/api/auth/google", "_self")
   };
 
   return (
-    <div className="p-[32px] mx-auto bg-tan min-w-screen min-h-screen flex flex-col">
+    <div className="p-[32px] mx-auto bg-tan min-w-screen min-h-screen flex font-[lexend] flex-col">
       <NavBar />
 
-      {/* Sign in with Google */}
-      <div className="flex-1 flex justify-center items-center">
+      <div className="flex-1 flex flex-col items-center justify-center text-center px-8">
+      <div className="bg-ash p-6 rounded-lg shadow-md w-full max-w-xl">
+        <h2 className="text-2xl font-semibold text-[#305252] mb-4">
+          UCLA Login Required
+        </h2>
+        <p className="text-gray-700 mb-6">You must be signed in with a UCLA email.</p>
         <button
-          onClick={handleGoogleLogin}
-          className="bg-slate text-white px-[16px] py-[8px] rounded"
-        >
-          Sign in with Google
+                onClick={handleGoogleLogin}
+                className="bg-slate text-white px-[16px] py-[8px] rounded">
+                Sign in with Google
         </button>
       </div>
-    </div>
+  </div>
+</div>
   );
 };
 
